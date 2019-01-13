1d1fd2423e7517bf2b0423451b9e6804bc5235c4
aditya0212jain <aditya0212jain@gmail.com>
2019-01-13 06:03:27 -0800

    uploaded with returning

--- MyBot.java
+++ MyBot.java
@@ -5,8 +5,13 @@ import hlt.*;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.LinkedHashMap;
+import java.util.Map;
+// import java.util.Dictionary;
 
 public class MyBot {
+
+
     public static void main(final String[] args) {
         final long rngSeed;
         if (args.length > 1) {
@@ -20,9 +25,11 @@ public class MyBot {
         // At this point "game" variable is populated with initial map data.
         // This is a good place to do computationally expensive start-up pre-processing.
         // As soon as you call "ready" function below, the 2 second per turn timer will start.
-        game.ready("MyJavaBot");
+        game.ready("Aditya's 0.0");
 
-        Log.log("Successfully created bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
+        Log.log("Successfully created Aditya 0.0 bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
+
+        final Map<Ship,Boolean> ships_exploring_status = new LinkedHashMap<>();
 
         for (;;) {
             game.updateFrame();
@@ -32,6 +39,23 @@ public class MyBot {
             final ArrayList<Command> commandQueue = new ArrayList<>();
 
             for (final Ship ship : me.ships.values()) {
+
+                //Below contains the returning code for ships
+                if(ships_exploring_status.get(ship)==null){
+                    ships_exploring_status.put(ship,true);
+                }
+                if(ships_exploring_status.get(ship)==false){
+                    if(ship.position==me.shipyard.position){
+                        ships_exploring_status.put(ship,true);
+                    }else{
+                        commandQueue.add(ship.move(gameMap.naiveNavigate(ship, me.shipyard.position)));
+                        continue;
+                    }
+                }else if(ships_exploring_status.get(ship)==true && ship.halite >= Constants.MAX_HALITE/2){
+                    ships_exploring_status.put(ship,false);
+                }
+                //Above contains the returning code
+
                 if (gameMap.at(ship).halite < Constants.MAX_HALITE / 10 || ship.isFull()) {
                     final Direction randomDirection = Direction.ALL_CARDINALS.get(rng.nextInt(4));
                     commandQueue.add(ship.move(randomDirection));
@@ -40,13 +64,14 @@ public class MyBot {
                 }
             }
 
-            if (
-                game.turnNumber <= 200 &&
-                me.halite >= Constants.SHIP_COST &&
-                !gameMap.at(me.shipyard).isOccupied())
-            {
-                commandQueue.add(me.shipyard.spawn());
-            }
+            //below code spawns a new ship if we have sufficient halite and shipyard is not occupied
+            // if (
+            //     game.turnNumber <= 200 &&
+            //     me.halite >= Constants.SHIP_COST &&
+            //     !gameMap.at(me.shipyard).isOccupied())
+            // {
+            //     commandQueue.add(me.shipyard.spawn());
+            // }
 
             game.endTurn(commandQueue);
         }
--- run_game.sh
+++ run_game.sh
@@ -3,4 +3,5 @@
 set -e
 
 javac MyBot.java
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java MyBot"
+javac initialBot.java
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "java MyBot" "java initialBot"
