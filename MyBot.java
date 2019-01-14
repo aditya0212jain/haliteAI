dc25b76aaad96c21528ad8d88336602730df5cb6
aditya0212jain <aditya0212jain@gmail.com>
2019-01-14 09:41:33 -0800

    blocking others path added

--- MyBot.java
+++ MyBot.java
@@ -10,10 +10,6 @@ import java.util.Map;
 // import java.util.Dictionary;
 
 public class MyBot {
-
-    // public Direction withoutCollision(Ship ship, Player me, GameMap gameMap){
-
-    // }
     
     public static ArrayList<Position> neighbours(Position pos,GameMap gameMap){
         ArrayList<Position> ans = new ArrayList<>();
@@ -24,6 +20,53 @@ public class MyBot {
             ans.add(newPosition);
         }
         return ans;
+    }
+
+    public static int optimalMoveHelper(int depth,Position pos,GameMap gameMap,Map<Position,Boolean> visited_position){
+        if(depth==0){
+            // if(visited_position.get(pos)==null||visited_position.get(pos)==false){
+            //     visited_position.put(pos,true);
+                return gameMap.at(pos).halite;
+            // }else{
+            //     return 0;
+            // }
+        }
+        else{
+            int max = 0;
+            // visited_position.put(pos,true);
+            ArrayList<Position> nei = neighbours(pos,gameMap);
+            for(int i=0;i<nei.size();i++){
+                // if(visited_position.get(nei.get(i))==null||visited_position.get(nei.get(i))==false){
+                    // visited_position.put(nei.get(i),true);
+                    int value = optimalMoveHelper(depth-1,nei.get(i),gameMap,visited_position);
+                    if(value>=max){
+                        max = value;
+                    }
+                // }
+            }
+            return max;
+        }
+    }
+    // -1 for stay still and 0,1,2,3 for respective directions
+    public static int optimalMove(int depth,Position pos, Ship ship , GameMap gameMap){
+        ArrayList<Position> nei = neighbours(pos,gameMap);
+        Map<Position,Boolean> visited_position = new LinkedHashMap<>();
+        visited_position.put(pos,true);
+        int max = 0;
+        int opM = 0;
+        for(int i=0;i<nei.size();i++){
+            int value = optimalMoveHelper(depth-1,nei.get(i),gameMap,visited_position);
+            Log.log("value :"+Integer.toString(value));
+            if(value>=max){
+                opM = i;
+                max = value;
+            }
+        }
+        // if(max<=gameMap.at(pos).halite){
+        //     return -1;
+        // }
+        Log.log("optimal value:  "+Integer.toString(opM));
+        return opM;
     }
 
     public static void main(final String[] args) {
@@ -46,12 +89,15 @@ public class MyBot {
 
         }
         int number_of_dropoff = 0;
-        int ships_limit=10;
+        int ships_limit=8;
+        int depth_search = 1;
+        int number_blocking_ships =0;
         game.ready("Aditya's 0.0");
 
         Log.log("Successfully created Aditya 0.0 bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
 
         final Map<EntityId,Boolean> ships_exploring_status = new LinkedHashMap<>();
+        final Map<Integer,Boolean> block_others_ship = new LinkedHashMap<>();
 
         for (;;) {
             game.updateFrame();
@@ -63,7 +109,7 @@ public class MyBot {
             final ArrayList<Command> commandQueue = new ArrayList<>();
 
             final Map<Position,Boolean> occupied_position = new LinkedHashMap<>();
-            ships_limit = 10 + (3*me.dropoffs.size());
+            ships_limit = 12 + (2*me.dropoffs.size());
 
             for (final Ship ship : me.ships.values()){
                 occupied_position.put(ship.position,true);
@@ -73,12 +119,45 @@ public class MyBot {
             for (final Ship ship : me.ships.values()) {
                 //For each ship do the following
                 //---------------------------------------------------------------------------------------------------------------------------------
+                if(number_blocking_ships<1){
+                    block_others_ship.put(ship.id.id,true);
+                    number_blocking_ships += 1;
+                }
+                if(block_others_ship.get(ship.id.id)==null||block_others_ship.get(ship.id.id)==false){
 
-                Log.log(Integer.toString(ship.halite)+ " ship's halite");
+                }else{
+                    int playerToBlock=0;
+                    boolean temp1 = true;
+                    for(int i=0;i<game.players.size() && temp1;i++){
+                        if(me.id.id!=game.players.get(i).id.id){
+                            playerToBlock = i;
+                            temp1 = false;
+                        }
+                    }
+                    Position finalDrop = game.players.get(playerToBlock).shipyard.position;
+                    Direction temp = gameMap.naiveNavigate(ship,finalDrop);
+                    Position newPosition = ship.position.directionalOffset(temp);
+                    newPosition = gameMap.normalize(newPosition);
+                    if(ship.halite < gameMap.at(ship).halite/10 && gameMap.at(ship).halite!=0){
+                        commandQueue.add(ship.stayStill());
+                        continue;
+                    }
+                    if(occupied_position.get(newPosition)==null||occupied_position.get(newPosition)==false||gameMap.at(newPosition).isOccupied()==false){
+                        occupied_position.put(newPosition,true);
+                        occupied_position.put(ship.position,false);
+                        commandQueue.add(ship.move(temp));
+                    }else{
+                        commandQueue.add(ship.stayStill());
+                    }
+                    continue;
+                }
+
+                // Log.log(Integer.toString(ship.halite)+ " ship's halite");
                 if(ships_exploring_status.get(ship.id)==null){
                     ships_exploring_status.put(ship.id,true);
                 }
                 if(ships_exploring_status.get(ship.id)==false){
+                    //if ships is returning------------------------------------
                     if(ship.halite==0){
                         if(gameMap.at(me.shipyard).isOccupied() && gameMap.at(me.shipyard).ship.id.id == ship.id.id){
                             ships_exploring_status.put(ship.id,true);
@@ -94,11 +173,11 @@ public class MyBot {
                         if(game.turnNumber<=400 && me.dropoffs.size()<4){
                             boolean farEnoughPoint = true;
                             for(Dropoff drop : me.dropoffs.values()){
-                                if(gameMap.calculateDistance(ship.position,drop.position)<10){
+                                if(gameMap.calculateDistance(ship.position,drop.position)<15){
                                     farEnoughPoint = false;
                                 }
                             }
-                            if(gameMap.calculateDistance(ship.position,me.shipyard.position)<10){
+                            if(gameMap.calculateDistance(ship.position,me.shipyard.position)<15){
                                 farEnoughPoint = false;
                             }
                             if(farEnoughPoint){
@@ -139,11 +218,11 @@ public class MyBot {
                 //Above contains the returning code
                 //sufficient halite to move forward
                 if(ship.halite < gameMap.at(ship).halite/10 && gameMap.at(ship).halite!=0){
-                    Log.log("8");
                     commandQueue.add(ship.stayStill());
                     continue;
                 }
 
+                //getting out of stuck position at shipyard
                 if(gameMap.at(me.shipyard).isOccupied() && gameMap.at(me.shipyard).ship.id.id == ship.id.id){
                     boolean forThisIf = true;
                     for(int i=0;i<4 && forThisIf;i++){
@@ -166,21 +245,11 @@ public class MyBot {
 
                 if (gameMap.at(ship).halite < Constants.MAX_HALITE / 10 || ship.isFull()) {
                     //introducing code for moving in the cell with max halite
-                    int toMove =rng.nextInt(4);
-                    int max = 0;
-                    for(int i=0;i<4;i++){
-                        final Direction dir = Direction.ALL_CARDINALS.get(i);
-                        Position newPosition = ship.position.directionalOffset(dir);
-                        newPosition = gameMap.normalize(newPosition);
-                        int haliteHere = gameMap.at(newPosition).halite;
-                        Log.log(Integer.toString(haliteHere)+" halite here");
-                        if(max<haliteHere){
-                            max = haliteHere;
-                            toMove = i;
-                        }
-                    }
                     
-                    final Direction dirToMove = Direction.ALL_CARDINALS.get(toMove);
+                    int a = optimalMove(depth_search,ship.position,ship,gameMap);
+                
+                    
+                    final Direction dirToMove = Direction.ALL_CARDINALS.get(a);
                     
                     Position newPosition = ship.position.directionalOffset(dirToMove);
                     newPosition = gameMap.normalize(newPosition);
@@ -191,6 +260,7 @@ public class MyBot {
                     }else{
                         commandQueue.add(ship.stayStill());
                     }
+                    
                     //above code represents the max move
                 } else {
                     commandQueue.add(ship.stayStill());
@@ -209,13 +279,13 @@ public class MyBot {
                 return_halite_limit = 500;
             }
             if(game.turnNumber == 200){
-                return_halite_limit = 700;
+                return_halite_limit = 900;
             }
             if(game.turnNumber == 300){
                 return_halite_limit = 800;
             }
             if(game.turnNumber == 400){
-                return_halite_limit = 800;
+                return_halite_limit = 700;
             }
 
 
