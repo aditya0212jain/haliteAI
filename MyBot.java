ff395d1e84c71610c95e787e872ea3408604126c
aditya0212jain <aditya0212jain@gmail.com>
2019-01-14 04:17:52 -0800

    removed stuck at shipyard bug

--- MyBot.java
+++ MyBot.java
@@ -33,7 +33,7 @@ public class MyBot {
 
         Log.log("Successfully created Aditya 0.0 bot! My Player ID is " + game.myId + ". Bot rng seed is " + rngSeed + ".");
 
-        final Map<Ship,Boolean> ships_exploring_status = new LinkedHashMap<>();
+        final Map<EntityId,Boolean> ships_exploring_status = new LinkedHashMap<>();
 
         for (;;) {
             game.updateFrame();
@@ -48,18 +48,32 @@ public class MyBot {
 
             for (final Ship ship : me.ships.values()){
                 occupied_position.put(ship.position,true);
+                Log.log("1");
             }
+            
 
             for (final Ship ship : me.ships.values()) {
-
+                Log.log(Integer.toString(ship.halite)+ " ship's halite");
+                // if(gameMap.at(me.shipyard).isOccupied()){
+                //     Log.log("shipyard2");
+                //     int a = gameMap.at(me.shipyard).ship.id.id;
+                //     Log.log(Integer.toString(a));
+                //     Log.log(Integer.toString(ship.id.id));
+                //     if(gameMap.at(me.shipyard).ship.id.id == ship.id.id){
+                //         Log.log("shipyard");
+                //     }
+                // }
                 //Below contains the returning code for ships
-                if(ships_exploring_status.get(ship)==null){
-                    ships_exploring_status.put(ship,true);
+                if(ships_exploring_status.get(ship.id)==null){
+                    Log.log("2");
+                    ships_exploring_status.put(ship.id,true);
                 }
-                if(ships_exploring_status.get(ship)==false){
-                    if(ship.position==me.shipyard.position){
-                        ships_exploring_status.put(ship,true);
+                if(ships_exploring_status.get(ship.id)==false){
+                    if(gameMap.at(me.shipyard).isOccupied() && gameMap.at(me.shipyard).ship.id.id == ship.id.id){
+                        Log.log("3");
+                        ships_exploring_status.put(ship.id,true);
                     }else{
+                        Log.log("4");
                         Direction temp = gameMap.naiveNavigate(ship,me.shipyard.position);
                         Position newPosition = ship.position.directionalOffset(temp);
                         newPosition = gameMap.normalize(newPosition);
@@ -67,21 +81,41 @@ public class MyBot {
                             occupied_position.put(newPosition,true);
                             occupied_position.put(ship.position,false);
                             commandQueue.add(ship.move(temp));
+                            Log.log("5");
                         }else{
+                            Log.log("6");
                             commandQueue.add(ship.stayStill());
                         }
                         continue;
                     }
-                }else if(ships_exploring_status.get(ship)==true && ship.halite >= 600){//Constants.MAX_HALITE
-                    ships_exploring_status.put(ship,false);
+                }else if(ships_exploring_status.get(ship.id)==true && ship.halite >= 600){//Constants.MAX_HALITE
+                    Log.log("7");
+                    ships_exploring_status.put(ship.id,false);
                 }
                 //Above contains the returning code
 
+                //below if ship is stuck at shipyard
+                // if(ship.position == me.shipyard.position){
+                //     final Direction randomDirection = Direction.ALL_CARDINALS.get(rng.nextInt(4));
+                //     Position newPosition = ship.position.directionalOffset(randomDirection);
+                //     newPosition = gameMap.normalize(newPosition);
+                //     if(occupied_position.get(newPosition)==null||occupied_position.get(newPosition)==false){
+                //         occupied_position.put(newPosition,true);
+                //         occupied_position.put(ship.position,false);
+                //         commandQueue.add(ship.move(randomDirection));
+                //     }else{
+                //         //else move to the position and swap occupied positions
+                //         commandQueue.add(ship.stayStill());
+                //     }
+                //     continue;
+                // }
                 //sufficient halite to move forward
-                if(ship.halite < gameMap.at(ship).halite/10){
+                if(ship.halite < gameMap.at(ship).halite/10 && gameMap.at(ship).halite!=0){
+                    Log.log("8");
                     commandQueue.add(ship.stayStill());
                     continue;
                 }
+
 
                 if (gameMap.at(ship).halite < Constants.MAX_HALITE / 10 || ship.isFull()) {
                     //introducing code for moving in the cell with max halite
@@ -100,6 +134,7 @@ public class MyBot {
                     }
                     Log.log("max halite : "+ Integer.toString(max));
                     final Direction dirToMove = Direction.ALL_CARDINALS.get(toMove);
+                    // final Direction randomDirection = Direction.ALL_CARDINALS.get(rng.nextInt(4));
                     Position newPosition = ship.position.directionalOffset(dirToMove);
                     newPosition = gameMap.normalize(newPosition);
                     if(occupied_position.get(newPosition)==null||occupied_position.get(newPosition)==false){
@@ -121,14 +156,9 @@ public class MyBot {
             //below code spawns a new ship if we have sufficient halite and shipyard is not occupied
             if(game.turnNumber <=300 ){
                 if(me.halite>=Constants.SHIP_COST && !gameMap.at(me.shipyard).isOccupied()){
-                    if(me.halite>=4000 && number_of_ships<1){
-
+                    if(me.halite>=4000 && number_of_ships<3){
                         commandQueue.add(me.shipyard.spawn());
                     }
-                    // if(me.halite >=2000){
-                        // commandQueue.add(me.shipyard.spawn());
-                        // occupied_position.put(me.shipyard.position,true);
-                    // }
                 }
             }
 
